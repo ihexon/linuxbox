914a4089dc1e5c6e4b74c97052831708bc7295ce
ihexon <14349453+ihexon@users.noreply.github.com>
2024-09-22 03:42:14 +0800

    feat(vhdx): vhdx.Create code from ovm-win

---
+++ pkg/machine/wsl/vhdx/vhdx.go
@@ -0,0 +1,39 @@
+// SPDX-FileCopyrightText: 2024 OOMOL, Inc. <https://www.oomol.com>
+// SPDX-License-Identifier: MPL-2.0
+
+package vhdx
+
+import (
+	"bauklotze/pkg/machine/machineDefine"
+	"fmt"
+	"syscall"
+)
+import "github.com/Microsoft/go-winio/vhd"
+
+// CreateVirtualDiskFlagSupportSparseFileAnyFs
+//
+// winio lacks this flag
+const (
+	// Ref: https://github.com/microsoft/win32metadata/blob/19ceee6047a3f083bbf573400ef8596ea66ad2d1/generation/WinSDK/RecompiledIdlHeaders/um/virtdisk.h#L382-L386
+	createVirtualDiskFlagSupportSparseFileAnyFs        = 0x400
+	blockSizeInMb                               uint32 = 1
+)
+
+// Create vhdx
+func Create(path machineDefine.VMFile, maxSizeInBytes uint64) error {
+	params := vhd.CreateVirtualDiskParameters{
+		Version: 2,
+		Version2: vhd.CreateVersion2{
+			MaximumSize:      maxSizeInBytes,
+			BlockSizeInBytes: blockSizeInMb * 1024 * 1024,
+		},
+	}
+
+	// Use `CreateVirtualDiskFlagSparseFile|createVirtualDiskFlagSupportSparseFileAnyFs` to create a sparse file,
+	// support dynamic size (automatic shrinking)
+	handle, err := vhd.CreateVirtualDisk(path.GetPath(), vhd.VirtualDiskAccessNone, vhd.CreateVirtualDiskFlagSparseFile|createVirtualDiskFlagSupportSparseFileAnyFs, &params)
+	if err != nil {
+		return fmt.Errorf("failed to create virtual disk: %w", err)
+	}
+	return syscall.CloseHandle(handle)
+}
